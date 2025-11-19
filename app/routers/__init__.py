21e5115098b246a1a0d779d9b2bbca8af4517799
Mauricio Atoy <javierllanosatoy@gmail.com>
2025-11-19 14:48:36 -0500

    Definir __all__ para controlar las exportaciones

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -4,11 +4,16 @@ Contiene los endpoints organizados por recurso.
 """
 
 # TODO: Importar los routers para facilitar su uso en main.py
-# from .usuarios import router as usuarios_router
-# from .peliculas import router as peliculas_router
-# from .favoritos import router as favoritos_router
+from .usuarios import router as usuarios_router
+from .peliculas import router as peliculas_router
+from .favoritos import router as favoritos_router
 
 # TODO: Opcional - Definir __all__ para controlar las exportaciones
-# __all__ = [
-# ]
+__all__ = [
+    "Usuario",
+    "Pelicula",
+    "Favorito",
+    "crear_usuario",
+    "obtener_pelicula",
+]
 
