f8167fa6be78e9cdba45c48484c697347d8dc7d3
Mauricio Atoy <javierllanosatoy@gmail.com>
2025-11-19 17:01:56 -0500

    Configurar el servidor uvicorn con los parámetros apropi

--- main.py
+++ main.py
@@ -124,5 +124,11 @@ if __name__ == "__main__":
     
     uvicorn.run(
         # TODO: Configurar el servidor uvicorn con los parámetros apropiados
+        "main:app",         
+        host="0.0.0.0", 
+        port=8000,
+        reload=True,
+        workers=1,
+        log_level="info",
     )
 
