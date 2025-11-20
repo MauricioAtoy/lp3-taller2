c46fc54f0ddc0bbf4e299133cb05899818617c13
alexander1143 <luisvalencia1143@gmail.com>
2025-11-19 19:15:40 -0500

    importar componentes

--- app/__init__.py
+++ app/__init__.py
@@ -5,14 +5,19 @@ Este módulo inicializa el paquete y expone los componentes principales.
 
 # TODO: Importar los componentes principales para facilitar su uso
 # Ejemplo:
-# from .database import get_session, engine
-# from .models import Usuario, Pelicula, Favorito
-# from .config import settings
+from .database import get_session, engine, DatabaseSession, check_database_connection
+from .models import Usuario, Pelicula, Favorito
+from .config import settings
 
 __version__ = "1.0.0"
 __author__ = "Tu Nombre"  # TODO: Reemplazar con tu nombre
 
 # TODO: Opcional - Definir __all__ para controlar qué se exporta
-# __all__ = [
-# ]
+__all__ = [
+    "Usuario",
+    "Pelicula",
+    "Favorito",
+    "crear_usuario",
+    "obtener_pelicula",
+]
 
